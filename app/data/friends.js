54e6dee91444d4b4918b627e46d7f53e34ceb7d0
Christian Escobar <crs.escobar@gmail.com>
2018-02-12 15:15:00 -0800

    various changes to pages and api route logic

---
+++ app/data/friends.js
@@ -0,0 +1,9 @@
+// Array has been pre-populated with dummy data
+
+let dataArray = [{
+	name: "Hector",
+  photo: "http://bit.ly/2Hb6OG2",
+  "scores": [ "1", "1", "2", "5", "5", "4", "3", "2", "5", "5" ]
+}];
+
+module.exports = dataArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,9 +1,36 @@
+const friends = require("../data/friends");
+
 module.exports = (app) => {
 	app.get("/api/friends", (req, res) => {
-		console.log("api/friends GET");
+		res.json(friends);
 	});
 
 	app.post("/api/friends", (req, res) => {
-		console.log("api/friends POST");
+		let currentMatch = null;
+		let bestDiffFound = 0;
+
+		for(let i=0; i<friends.length; i++) {
+			let currentFriendObj = friends[i];
+			// compare scores
+			let totalDiffForCurrentFriend = 0;
+			for(let j=0; j<req.body.scores.length; j++) {
+				totalDiffForCurrentFriend += Math.abs(currentFriendObj.scores[j] - req.body.scores[j]);
+			}
+
+			// When an existing friend has been compared, compare the total diff with the best diff so far
+			if(currentMatch === null) {
+				bestDiffFound = totalDiffForCurrentFriend;
+
+				currentMatch = currentFriendObj;
+			} else if(totalDiffForCurrentFriend > bestDiffFound) {
+				currentMatch = currentFriendObj;
+			}
+		}
+
+		friends.push(req.body);
+
+		// respond with a friend object
+		res.json(currentMatch);
 	});
 }
+
