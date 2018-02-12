12259a0fd7e6c46ab621eb78e8b26feb8417b2c6
Christian Escobar <crs.escobar@gmail.com>
2018-02-11 19:10:34 -0800

    initial server configuration

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+	console.log("Server started, listening on port", PORT);
+})
