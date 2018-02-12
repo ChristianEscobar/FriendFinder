986a03b5f4a5fa5fae918d6431139b69d9698705
Christian Escobar <crs.escobar@gmail.com>
2018-02-12 11:32:21 -0800

    various changes to pages

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const htmlRoutes = require("./app/routing/htmlRoutes");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
 apiRoutes(app);
 htmlRoutes(app);
 
