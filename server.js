2fefec4b19e7c581d69337e20c36676d8a47f701
Christian Escobar <crs.escobar@gmail.com>
2018-02-13 14:00:02 -0800

    work in progress for css MIME type bug

--- server.js
+++ server.js
@@ -2,18 +2,23 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const apiRoutes = require("./app/routing/apiRoutes");
 const htmlRoutes = require("./app/routing/htmlRoutes");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Middleware
+app.use(express.static(path.join(__dirname, "/public")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
+
+// Routes
 apiRoutes(app);
 htmlRoutes(app);
 
 app.listen(PORT, () => {
 	console.log("Server started, listening on port", PORT);
-})
+});
